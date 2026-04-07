dc1e24b218e9c685762151571e6c1bd195140e5d
Valerius Petrini <valeriuspetrini@gmail.com>
2026-04-07 19:02:21 +0000

    feat: enhance Progress component with animation controls and user inputs

--- src/routes/progress/+page.svelte
+++ src/routes/progress/+page.svelte
@@ -1,16 +1,41 @@
 <script lang="ts">
+    import Checkbox from "../../components/Checkbox.svelte";
     import FloatingInput from "../../components/FloatingInput.svelte";
     import Progress from "../../components/Progress.svelte";
 
     let value: number = $state(100);
+    let animate: boolean = $state(false);
+    let animateFrom: number = $state(0);
+    let animateTo: number = $state(100);
+    let animationDuration: number = $state(1000);
+    let animateOnEndAlert: string = $state("");
 </script>
 
 <div class="w-full h-screen flex pt-20">
     <div class="w-3/4 flex-center">
-        <Progress animate={{ from: 0, to: value, duration: 5000 }}/>
+        {#key animate}
+            <Progress 
+                {value} 
+                animate={animate 
+                    ? { 
+                        from: animateFrom, 
+                        to: animateTo, 
+                        duration: animationDuration, 
+                        onend: () => {animateOnEndAlert && alert(animateOnEndAlert)} 
+                      } 
+                    : undefined}
+            />
+        {/key}
     </div>
 
     <div class="w-1/4 p-4 flex flex-col gap-2">
         <FloatingInput size="full" label="Value" type="number" min="0" max="100" bind:value={value}/>
+        <Checkbox bind:checked={animate} label="Animate"/>
+        {#if animate}
+            <FloatingInput size="full" label="From" type="number" min="0" max="100" bind:value={animateFrom}/>
+            <FloatingInput size="full" label="To" type="number" min="0" max="100" bind:value={animateTo}/>
+            <FloatingInput size="full" label="Duration" type="number" min="0" bind:value={animationDuration}/>
+            <FloatingInput size="full" label="On End Alert" type="text" placeholder="Alert message on animation end" bind:value={animateOnEndAlert}/>
+        {/if}
     </div>
 </div>
