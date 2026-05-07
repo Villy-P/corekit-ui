34cd723c71c01a679be79a90668eb1fb889648be
Valerius Petrini <valeriuspetrini@gmail.com>
2026-05-07 19:06:45 +0000

    feat: add KBD component stories for Storybook

---
+++ src/lib/components/display/KBD/index.stories.svelte
@@ -0,0 +1,13 @@
+<script module lang="ts">
+	import { defineMeta } from "@storybook/addon-svelte-csf";
+    import KBD from "./index.svelte";
+	import { colorStyles, colorStyleTypes } from "$lib/styles/color.js";
+    import { sizeStyles } from "$lib/styles/size.js";
+
+	const { Story } = defineMeta({
+		title: "Components/Display/KBD",
+		component: KBD,
+	});
+</script>
+
+<Story name="Default">Ctrl</Story>
