b3fff919bee1467e5f36b6234fae9e40f0d69ab1
Villy-P <valeriuspetrini@gmail.com>
2026-05-20 14:58:44 -0500

    chore: update version to 0.1.78 in package.json and package-lock.json feat: add Textarea component and its props to inputs index

--- src/lib/components/inputs/index.ts
+++ src/lib/components/inputs/index.ts
@@ -5,6 +5,7 @@ export { default as Select } from "./Select/index.svelte";
 export { default as FileInput } from "./FileInput/index.svelte";
 export { default as Combobox } from "./Combobox/index.svelte";
 export { default as ColorInput } from "./ColorInput/index.svelte";
+export { default as Textarea } from "./Textarea/index.svelte";
 
 export type { ButtonProps } from "./Button/types.ts";
 export type { CheckboxProps } from "./Checkbox/types.ts";
@@ -13,3 +14,4 @@ export type { SelectProps } from "./Select/types.ts";
 export type { FileInputProps } from "./FileInput/types.ts";
 export type { ComboboxProps } from "./Combobox/types.ts";
 export type { ColorInputProps } from "./ColorInput/types.ts";
+export type { TextareaProps } from "./Textarea/types.ts";
