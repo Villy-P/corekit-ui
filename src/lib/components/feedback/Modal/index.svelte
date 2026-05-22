85b3440784b6bde1c9f411bd75c516a714b4aa92
Villy-P <valeriuspetrini@gmail.com>
2026-05-22 12:26:08 -0500

    fix: refactor Modal component to simplify open/close logic and improve accessibility

--- src/lib/components/feedback/Modal/index.svelte
+++ src/lib/components/feedback/Modal/index.svelte
@@ -9,25 +9,12 @@
     let { 
         children = undefined, 
         class: className = "",
-        element = $bindable<HTMLDialogElement>(),
-        open = $bindable(false), 
+        element = $bindable(),
+        open = $bindable(), 
         position = "center",
         size = "md",
         ...restProps
     }: ModalProps = $props();
-
-    $effect(() => {
-        if (!element) return;
-        if (open) {
-            (element as HTMLDialogElement).showModal();
-        } else {
-            (element as HTMLDialogElement).close();
-        }
-    });
-
-    function onclose() {
-        open = false;
-    }
 
     const outerClass = $derived(twMerge(
         "inset-0 flex flex-col w-[calc(100%-1rem)] h-[calc(100%-1rem)] p-4",
@@ -41,19 +28,20 @@
     ));
 </script>
 
-<dialog
-    bind:this={element}
-    class="w-full h-screen bg-black/50 z-300 fixed top-0 left-0 p-0 m-0 max-w-none max-h-none"
-    transition:fade={{ duration: 200 }}
-    onclick={() => open = false}
-    onclose={onclose}
-    {...restProps}
->
-    {#if open}
-        <div class={outerClass} transition:fly={{ y: -20, duration: 200 }}>
-            <Card class={cardClass} onclick={(e: MouseEvent) => e.stopPropagation()}>
-                {@render children?.()}
+{#if open}
+    <!-- svelte-ignore a11y_click_events_have_key_events -->
+    <!-- svelte-ignore a11y_no_static_element_interactions -->
+    <div
+        class="w-full h-screen bg-black/50 z-300 fixed top-0 left-0"
+        transition:fade={{ duration: 200 }}
+        onclick={() => open = false}
+        bind:this={element}>
+        <div
+            class={outerClass}
+            transition:fly={{ y: -20, duration: 200 }}>
+            <Card class={cardClass} {...restProps} onclick={(e: MouseEvent) => e.stopPropagation()}>
+                {@render children()}
             </Card>
         </div>
-    {/if}
-</dialog>
+    </div>
+{/if}
