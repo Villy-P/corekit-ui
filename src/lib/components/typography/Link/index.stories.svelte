f7dd173035a11cb64c76bf015fd1250a5569c0b8
Villy-P <valeriuspetrini@gmail.com>
2026-05-28 23:34:34 -0500

    feat: add story to link component

---
+++ src/lib/components/typography/Link/index.stories.svelte
@@ -0,0 +1,16 @@
+<script module lang="ts">
+	import { defineMeta } from "@storybook/addon-svelte-csf";
+    import Link from "./index.svelte";
+
+	const { Story } = defineMeta({
+		title: "Components/Typography/Link",
+		component: Link,
+		argTypes: {
+			
+        }
+	});
+</script>
+
+<Story name="Default" args={{ href: "https://example.com" }}>
+    The quick brown fox jumps over the lazy dog.
+</Story>
