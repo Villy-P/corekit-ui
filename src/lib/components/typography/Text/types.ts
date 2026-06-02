44d297f3274ac1292ee9e6df0324d0d766b19c67
Villy-P <valeriuspetrini@gmail.com>
2026-06-02 12:40:05 -0500

    feat: update Text component to support 'ul' tag and adjust styles for 'li' elements

--- src/lib/components/typography/Text/types.ts
+++ src/lib/components/typography/Text/types.ts
@@ -1,7 +1,7 @@
 import type { SizeStyle } from "../../../styles/size.ts";
 import type { BaseComponentProps } from "../../../types/BaseComponent.ts";
 
-type TextTagOptions = "p" | "div" | "span" | "label" | "strong" | "em" | "b" | "i" | "u" | "small" | "blockquote" | "pre" | "code" | "a" | "li" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+type TextTagOptions = "p" | "div" | "span" | "label" | "strong" | "em" | "b" | "i" | "u" | "small" | "blockquote" | "pre" | "code" | "a" | "ul" | "li" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 
 export interface TextProps extends BaseComponentProps {
     tag?: TextTagOptions;
