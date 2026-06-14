210474d20433d7aa6f45cb959d27fbf9e6a205cc
Villy-P <valeriuspetrini@gmail.com>
2026-06-13 21:50:47 -0500

    feat: enhance Text component with improved argTypes, documentation, and type definitions for better customization

--- src/lib/components/typography/Text/index.stories.svelte
+++ src/lib/components/typography/Text/index.stories.svelte
@@ -1,13 +1,45 @@
 <script module lang="ts">
 	import { defineMeta } from "@storybook/addon-svelte-csf";
     import Text from "../../typography/Text/index.svelte";
+	import { sizeStyles } from "$lib/styles/size";
+	import { baseArgTypes } from "$lib/styles/storybook";
+
+	const argTypes = {
+		...baseArgTypes,
+		tag: {
+			control: "select",
+			options: ["p", "div", "span", "label", "strong", "em", "b", "i", "u", "small", "blockquote", "pre", "code", "a", "ul", "li", "h1", "h2", "h3", "h4", "h5", "h6"],
+			description: "The HTML tag to use for the text element.",
+			table: {
+				type: { summary: 'TextTagOptions' },
+				category: 'Appearance',
+			},
+		},
+		size: {
+			control: "select",
+			options: sizeStyles,
+			description: "The size of the text.",
+			table: {
+				type: { summary: 'Size' },
+				category: 'Appearance',
+			},
+		},
+	} as const;
 
 	const { Story } = defineMeta({
 		title: "Components/Typography/Text",
 		component: Text,
-		argTypes: {
-			
-        }
+		argTypes,
+		parameters: {
+			controls: {
+				include: Object.keys(argTypes),
+			},
+			docs: {
+				description: {
+					component: "A text component that can be used to display text with different HTML tags and sizes.",
+				},
+			},
+		},
 	});
 </script>
 
--- src/lib/components/typography/Text/types.ts
+++ src/lib/components/typography/Text/types.ts
@@ -1,9 +1,15 @@
+import type { HTMLAnchorAttributes, HTMLAttributes } from "svelte/elements";
 import type { Size } from "../../../styles/size.ts";
-import type { BaseComponentProps } from "../../../types/BaseComponent.ts";
+import type { BaseComponentProps, BaseProps } from "../../../types/BaseComponent.ts";
 
 type TextTagOptions = "p" | "div" | "span" | "label" | "strong" | "em" | "b" | "i" | "u" | "small" | "blockquote" | "pre" | "code" | "a" | "ul" | "li" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 
-export interface TextProps extends BaseComponentProps {
+export interface TextProps extends BaseProps, Omit<HTMLAttributes<HTMLElement>, keyof BaseProps> {
     tag?: TextTagOptions;
     size?: Size;
+
+    href?: string;
+    target?: string;
+    rel?: string;
+    for?: string;
 };  
